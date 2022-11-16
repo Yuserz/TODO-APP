31f7aad19a0e2dc45e38f2d766edef7f6b6d33b5
Yusri Caloyloy <61104011+Yuserz@users.noreply.github.com>
2022-11-16 08:03:03 +0000

    feat: add test

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import Fastify from 'fastify'; 
+import Fastify from 'fastify';
 
 async function start () {
+  try {
+    // if something goes wrong here, it will do the catch part
+    // Require the framework and instantiate it
+    const fastify = Fastify({ logger: true });
 
-    try {
-        //if something goes wrong here, it will do the catch part
-        // Require the framework and instantiate it
-        const fastify = Fastify({ logger: true })
+    // Declare a route
+    fastify.get('/api', async (request, reply) => {
+      return { success: true };
+    });
 
-        // Declare a route
-        fastify.get('/api', async (request, reply) => {
-        return { success: true }
-        })
+    const addr = await fastify.listen({
+      port: '8080'
+    });
 
-        const addr = await fastify.listen({
-            port: '8080'
-        })
-
-        console.log(`Listening in port ${addr}`);
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+    console.log(`Listening in port ${addr}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 start();
