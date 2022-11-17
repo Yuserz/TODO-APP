478177426576169d90c91acbba23ef4cdf3e402e
Yusri Caloyloy <61104011+Yuserz@users.noreply.github.com>
2022-11-17 02:14:04 +0000

    feat: add test and refactor fastify

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import Fastify from 'fastify';
+import { build } from './app.js';
 
 async function start () {
   try {
     // if something goes wrong here, it will do the catch part
-    // Require the framework and instantiate it
-    const fastify = Fastify({ logger: true });
-
-    // Declare a route
-    fastify.get('/api', async (request, reply) => {
-      return { success: true };
-    });
+    const fastify = await build();
 
     const addr = await fastify.listen({
       port: '8080'
