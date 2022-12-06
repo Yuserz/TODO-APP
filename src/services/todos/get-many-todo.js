7ebea9de3e8359799152548c329401259cf02db9
Yusri Caloyloy <yusripawaki2@gmail.com>
2022-12-06 07:24:43 +0000

    chore: bump

--- src/services/todos/get-many-todo.js
+++ src/services/todos/get-many-todo.js
@@ -7,29 +7,19 @@ export const getManyTodo = async (request, reply) => {
 
   const list = [];
 
-  const todos1 = Object
-    .entries(db.todos);
+  const todos = Object
+    .entries(db.todos)
+    .map(function ([id, todo]) {
+      return {
+        id,
+        ...todo
+      };
+    })
+    .sort(function (todo1, todo2) {
+      return todo2.createdDate - todo1.createdDate;
+    })
 
-  const todos2 = todos1.map(function ([id, todo]) {
-    return {
-      id,
-      ...todo
-    };
-  });
-
-  for(const{ title, createdDate } of todos2) {
-    console.log('unsorted', title, new Date(createdDate))
-  }
-
-  todos2.sort(function (todo1, todo2) {
-    return todo2.createdDate - todo1.createdDate;
-  });
-
-  for(const{ title, createdDate } of todos2) {
-    console.log('sorted', title, new Date(createdDate))
-  }
-
-  for (const todo of todos2) {
+  for (const todo of todos) {
     list.push(todo);
     if (list.length >= limit) {
       break;
